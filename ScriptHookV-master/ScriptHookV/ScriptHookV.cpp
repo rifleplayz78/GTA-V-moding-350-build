8d0284198b923399fcf6e853b166da9c76b2343c
RiflePlayz <ranaalihussainafzalkhan@gmail.com>
2026-09-05 11:40:09 +0500

    Update ScriptHookV.cpp

--- ScriptHookV-master/ScriptHookV/ScriptHookV.cpp
+++ ScriptHookV-master/ScriptHookV/ScriptHookV.cpp
@@ -24,10 +24,6 @@ BOOL APIENTRY DllMain( HINSTANCE hModule, DWORD dwReason, LPVOID /*lpvReserved*/
 			// no legals
 			if (auto p_gameLegals = "72 1F E8 ? ? ? ? 8B 0D"_Scan) p_gameLegals.nop(2);
 
-			// no annoying movie
-			// this doesn't save any loadup time just plays nothing
-			//if (auto p_preMovie = "70 6C 61 74 66 6F 72 6D 3A 2F 6D 6F 76"_Scan) p_preMovie.nop(13);
-
 			static auto& versionTool = GTAVersion::GetInstance();
 			g_GameVersion = versionTool.GameVersion();
 			auto gta5directory = versionTool.GameDirectory();
@@ -38,36 +34,54 @@ BOOL APIENTRY DllMain( HINSTANCE hModule, DWORD dwReason, LPVOID /*lpvReserved*/
 				auto file = GetOurModuleFolder() + "\\Native_Registration.txt";
 				if (DoesFileExist(file.c_str())) remove(file.c_str());
 				LOG_FILE("Native_Registration", "%s", versionString.c_str());
+				
 				CreateElevatedThread([](LPVOID)->DWORD
 				{ 
 					Hooking::HookFunctions();
 					while (true)
 					{
-						static eGameState* gameState = "83 3D ? ? ? ? ? 8A D9 74 0A"_Scan.add(2).rip(5).as<decltype(gameState)>();
-						if (*gameState == GameStateMainMenu)
+						// Added safety pattern verification to prevent crashing on unmapped versions
+						auto scanResult = "83 3D ? ? ? ? ? 8A D9 74 0A"_Scan;
+						if (scanResult)
 						{
-							Hooking::UnHookFunctions();
-							MessageBoxA(NULL, FMT("New Game Version %s", versionTool.VersionString().c_str()).c_str(), "ScriptHookV Incompatible", MB_OK | MB_TOPMOST);
+							static eGameState* gameState = scanResult.add(2).rip(5).as<decltype(gameState)>();
+							if (*gameState == GameStateMainMenu)
+							{
+								Hooking::UnHookFunctions();
+								MessageBoxA(NULL, FMT("New Game Version %s", GTAVersion::GetInstance().VersionString().c_str()).c_str(), "ScriptHookV Incompatible", MB_OK | MB_TOPMOST);
+								FreeLibraryAndExitThread(Utility::GetOurModuleHandle(), ERROR_SUCCESS);
+								break;
+							}
+						}
+						else
+						{
+							// Fallback failure exit if memory layout is completely unrecognized
+							MessageBoxA(NULL, "Unknown game memory layout detected.", "ScriptHookV Error", MB_OK | MB_TOPMOST);
 							FreeLibraryAndExitThread(Utility::GetOurModuleHandle(), ERROR_SUCCESS);
 							break;
 						}
+						Sleep(100); // Prevent burning CPU cores in an infinite loop
 					}
 					return TRUE;
 				});
 			}
 			else
 			{
-				if (g_IsRetail = !DoesFileExist((gta5directory + "\\steam_api64.dll").c_str())) g_GameVersion += 1;
+				g_IsRetail = !DoesFileExist((gta5directory + "\\steam_api64.dll").c_str());
+				
 				LOG_DEBUG("found GTA5 directory %s", gta5directory.c_str());
-				LOG_DEBUG("detected GTA5 %s Version %s (SHV patch %d)", g_IsRetail ? "Retail" : "Steam", versionString.c_str(), g_GameVersion);
+				LOG_DEBUG("detected GTA5 %s Version %s", g_IsRetail ? "Retail" : "Steam", versionString.c_str());
 
-				// incompatible with versions prior to 1.0.1493.0 with current hashmap.
-				if (g_GameVersion < VER_1_0_1493_0_STEAM)
+				// Adjusted minimum version boundary explicitly down to build 350
+				if (g_GameVersion < VER_1_0_350_1_STEAM)
 				{
-					MessageBoxA(NULL, "Update to Version 1.0.1493.0", "Game Version Incompatible!",  MB_OK | MB_TOPMOST);
+					MessageBoxA(NULL, "Update to Version 1.0.350.1 or higher.", "Game Version Incompatible!",  MB_OK | MB_TOPMOST);
 					FreeLibraryAndExitThread(hModule, 0);
 					return TRUE;
 				}
+
+				// If it is retail, we only increment AFTER the minimum version check has passed securely
+				if (g_IsRetail) g_GameVersion += 1;
 
 				CreateElevatedThread([](LPVOID)->DWORD
 				{
@@ -75,9 +89,9 @@ BOOL APIENTRY DllMain( HINSTANCE hModule, DWORD dwReason, LPVOID /*lpvReserved*/
 					{
 						LOG_ERROR("Failed to initialize ScriptEngine");
 						return FALSE;
-					}	return TRUE;
+					}	
+					return TRUE;
 				});
-				
 			}
 			break;
 		}
